cded27bf9a2a8e392b5b88ceb6aa8f41606360c9
Nathan Oliboni <116748360+NathanOliboni@users.noreply.github.com>
2023-02-17 16:02:10 -0300

    Update overflow.vhdl

--- overflow.vhdl
+++ overflow.vhdl
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity overflow is
     port (
-        a, b, z: in std_logic;
+        a, b, z, op: in std_logic;
         overflw : out std_logic
     );
 end entity;
@@ -11,6 +11,6 @@ end entity;
 architecture over of overflow is
 begin
 
-    overflw <= ((not a and not b and z) or (a and b and not z));
+    overflw <= (a and b and not op) or (z and not op) after 12 ns; 
     
 end over;
